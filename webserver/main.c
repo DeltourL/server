352e52b1149253acd875290128a46ab37bd65cd2
RST <rudy.16@hotmail.fr>
2016-03-18 11:37:16 +0100

    pfff pb curl tj

--- webserver/main.c
+++ webserver/main.c
@@ -7,6 +7,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+*
+*
+*
+*je comprend pas nc fonctionne nickel mais curl pas du tout sa fait + de 9h que je suis dessu !!!
+*
+*
+*
+*
+*
+*
+*/
+
 int compter_char(char * bufcpy,char c)
 {
 	int areturn=0;
@@ -47,7 +60,8 @@ int str_match(char * bufcpy)
 	return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	argc = argc;
 	argv = argv;//TODO temporaire
 
@@ -63,7 +77,8 @@ int main(int argc, char **argv) {
 	while(1) {  
     	/*fixe la file d'attente et demare l'écoute*/
     	int file_attente = 10;
-    	if( listen( serveur , file_attente) == -1) {
+    	if( listen( serveur , file_attente) == -1) 
+    	{
 			perror("listen socket_serveur");
 			return -3;
 		}
@@ -71,30 +86,33 @@ int main(int argc, char **argv) {
 		/*cree socket client*/
 		int socket_client ;
 		socket_client = accept(serveur, NULL, NULL);
-		if ( socket_client == -1) {
+		if ( socket_client == -1) 
+		{
 			perror ("accept");
 			return -4;
 		}
-		if (fork()==0) {
+		if (fork()==0) 
+		{
 			/* On peut maintenant dialoguer avec le client */
-			//const char * message_bienvenue = "0100001001101111011011100110101001101111011101010111001000100000001011000010000001100010011010010110010101101110011101100110010101101110011101010110010100100000011100110111010101110010001000000110110101101111011011100010000001110011011001010111001001110110011001010111010101110010\n102 157 156 152 157 165 162 054 040 142 151 145 156 166 145 156 165 145 040 163 165 162 040 155 157 156 040 163 145 162 166 145 165 162\n66 111 110 106 111 117 114 44 32 98 105 101 110 118 101 110 117 101 32 115 117 114 32 109 111 110 32 115 101 114 118 101 117 114\n42 6f 6e 6a 6f 75 72 2c 20 62 69 65 6e 76 65 6e 75 65 20 73 75 72 20 6d 6f 6e 20 73 65 72 76 65 75 72\nLXVI CXI CX CVI CXI CXVII CXIV XLIV XXXII XCVIII CV CI CX CXVIII CI CX CXVII CI XXXII CXV CXVII CXIV XXXII CIX CXI CX XXXII CXV CI CXIV CXVIII CI CXVII CXIV\n-... --- -. .--- --- ..- .-. --..-- / -... .. . -. ...- . -. ..- . / ... ..- .-. / -- --- -. / ... . .-. ...- . ..- .-.\nßo|\\|joµ12, b][env3nue su.- mo/V $&12\\/eur\n7752e0fad02e1ac9dbda87af611e6602\n79039ee375d4ec5cd4b8d29bdba19e279ee8c03e\nBonjour, bienvenue sur mon serveur\n";
-			
-			
 
 			char buf[255] = "a";
 			//char bufServer[110] = "<server>";
 			
 			FILE* file = fdopen(socket_client, "w+");
-			if (file == NULL) {
+			if (file == NULL) 
+			{
 					fprintf(stderr, "%s\n", "ERROR opening socket_client");
 					return -7;
-			} else {
+			} 
+			else 
+			{
 				int entete_ok=0;
 				int ligne_vide_detecter=0;
 				//tant que la co est valide
-				while(strlen(buf) != 0) {
-					bzero(buf,255);
-					if (fgets(buf, 255, file) == NULL) {
+				while(strlen(buf) != 0) 
+				{
+					if (fgets(buf, 255, file) == NULL) 
+					{
 						fprintf(stderr, "%s\n", "ERROR reading from socket_client");
 						return -5;
 					}
@@ -138,6 +156,7 @@ int main(int argc, char **argv) {
 						fprintf(stderr, "%s\n", "ERROR writing to socket_client");
 						return -6;
 					}*/
+					//bzero(buf,255);
 				}//une fois que le client deco
 			}
 			fclose(file);
